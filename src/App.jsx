a8efe43a05ecbbb3c4886810ff6302601e126388
leo <lcytc119@gmail.com>
2023-12-12 20:19:17 -0500

    Initial edit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='logo'>
+        <img src="https://static.vecteezy.com/system/resources/previews/022/892/565/original/eyelashes-logo-design-with-unique-style-for-woman-free-vector.jpg" alt="logo"/>
+      </div>
+      <div className='header'>
+      <button>Home</button>
+      <button>Gallery</button>
+      <button>Menu</button>
+      </div>
+    </>
+  )
+}
+
+export default App
